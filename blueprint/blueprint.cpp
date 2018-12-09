910d3f39f9c6232412ba3678730bed055fab49fa
Nick Thompson <ncthom91@gmail.com>
2018-12-09 10:21:33 -0500

    Copy some files over from my previous go at this

---
+++ blueprint/blueprint.cpp
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    blueprint.cpp
+    Created: 26 Nov 2018 3:19:03pm
+
+  ==============================================================================
+*/
+
+#ifdef BLUEPRINT_H_INCLUDED
+ /* When you add this cpp file to your project, you mustn't include it in a file where you've
+    already included any other headers - just put it inside a file on its own, possibly with your config
+    flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
+    header files that the compiler may be using.
+ */
+ #error "Incorrect use of the Blueprint cpp file"
+#endif
+
+
+#include "blueprint.h"
+
+#include "yoga/yoga/Utils.cpp"
+#include "yoga/yoga/YGConfig.cpp"
+#include "yoga/yoga/YGEnums.cpp"
+#include "yoga/yoga/YGFloatOptional.cpp"
+#include "yoga/yoga/YGLayout.cpp"
+#include "yoga/yoga/YGNode.cpp"
+#include "yoga/yoga/YGNodePrint.cpp"
+#include "yoga/yoga/YGStyle.cpp"
+#include "yoga/yoga/Yoga.cpp"
+
+duk_double_t dummy_get_now(void) {
+    /* Return a fixed time here as a dummy example. */
+    return -11504520000.0;
+}
+
+#include "duktape/src-noline/duktape.c"
+#include "duktape/extras/console/duk_console.c"
---
+++ blueprint/blueprint.h
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * This file declares a JUCE module for the shared library code implemented in
+ * this directory. The block below is read by the Projucer to automatically
+ * generate project code that uses the module. For details, see the
+ * JUCE Module Format.txt file in the JUCE directory.
+
+   BEGIN_JUCE_MODULE_DECLARATION
+    ID:                 blueprint
+    vendor:             Nick Thompson
+    version:            0.1.0
+    name:               Blueprint
+    description:        A React.js render backend targeting JUCE Components.
+    minimumCppStandard: 17
+    dependencies:       juce_core, juce_graphics, juce_gui_basics
+    searchpaths:        ./duktape/src-noline/
+   END_JUCE_MODULE_DECLARATION
+*******************************************************************************/
+
+#pragma once
+
+#define BLUEPRINT_H_INCLUDED
+
+
+#include <juce_core/juce_core.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+
+#include "yoga/yoga/Utils.h"
+#include "yoga/yoga/YGConfig.h"
+#include "yoga/yoga/YGEnums.h"
+#include "yoga/yoga/YGFloatOptional.h"
+#include "yoga/yoga/YGLayout.h"
+#include "yoga/yoga/YGMacros.h"
+#include "yoga/yoga/YGNode.h"
+#include "yoga/yoga/YGNodePrint.h"
+#include "yoga/yoga/YGStyle.h"
+#include "yoga/yoga/Yoga-internal.h"
+#include "yoga/yoga/Yoga.h"
+
+#include "duktape/src-noline/duktape.h"
+#include "duktape/extras/console/duk_console.h"
+#include "duktape/src-noline/duk_config.h"
+
+extern duk_double_t dummy_get_now(void);
+
+#define DUK_USE_DATE_GET_NOW(ctx) dummy_get_now()
+#define DUK_USE_DATE_GET_LOCAL_TZOFFSET(d)  0
