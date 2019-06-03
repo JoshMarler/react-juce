5377d3b1c71b1772f28a96ac898595aea8b7f52a
Nicholas Thompson <ncthom91@gmail.com>
2019-06-03 08:52:25 -0400

    Flesh out the help screen with real text

--- examples/BlueprintPlugin/Source/ui/src/Help.js
+++ examples/BlueprintPlugin/Source/ui/src/Help.js
@@ -3,23 +3,112 @@ import React, { Component } from 'react';
 import { View, Image, Text } from './Blueprint';
 
 
+function SectionHeader(props) {
+  return (
+    <View {...styles.sectionHeader} {...props}>
+      <Text {...styles.sectionHeaderText}>
+        {props.children}
+      </Text>
+    </View>
+  );
+}
+
 class Help extends Component {
   render() {
     return (
-      <View {...this.props}>
-        <Text color={Colors.STROKE}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et netus et malesuada fames ac turpis egestas sed. Maecenas sed enim ut sem viverra aliquet eget sit. Habitant morbi tristique senectus et netus et. Et malesuada fames ac turpis egestas maecenas pharetra convallis. Viverra mauris in aliquam sem fringilla. Sit amet commodo nulla facilisi nullam vehicula ipsum. Hendrerit gravida rutrum quisque non tellus orci. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Eu non diam phasellus vestibulum lorem sed risus. Nibh sed pulvinar proin gravida hendrerit. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada.
-
-Arcu cursus vitae congue mauris rhoncus aenean vel. Tincidunt ornare massa eget egestas purus viverra accumsan in. Tortor posuere ac ut consequat. Pellentesque dignissim enim sit amet venenatis urna cursus. Mi eget mauris pharetra et. Consectetur adipiscing elit ut aliquam. Ut pharetra sit amet aliquam id diam maecenas ultricies. Aenean et tortor at risus viverra adipiscing. Facilisi nullam vehicula ipsum a arcu. Morbi tincidunt augue interdum velit euismod in. Dictum sit amet justo donec enim diam vulputate. Ligula ullamcorper malesuada proin libero nunc consequat interdum. Quis hendrerit dolor magna eget est lorem. Sem viverra aliquet eget sit. Urna molestie at elementum eu facilisis.
-
-Velit ut tortor pretium viverra. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam. Ultricies mi quis hendrerit dolor magna eget. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est. Et egestas quis ipsum suspendisse ultrices. In nisl nisi scelerisque eu. Accumsan tortor posuere ac ut consequat. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Faucibus in ornare quam viverra orci sagittis eu. Risus quis varius quam quisque. Adipiscing tristique risus nec feugiat in fermentum posuere urna nec. At urna condimentum mattis pellentesque id nibh tortor. Facilisi etiam dignissim diam quis. Molestie at elementum eu facilisis sed odio morbi quis. Sagittis nisl rhoncus mattis rhoncus urna neque. Fames ac turpis egestas sed tempus urna. Ipsum faucibus vitae aliquet nec ullamcorper sit amet.
-
-Rutrum quisque non tellus orci. Cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Quam id leo in vitae. Aliquam sem fringilla ut morbi. Interdum varius sit amet mattis vulputate. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Molestie nunc non blandit massa. Euismod quis viverra nibh cras pulvinar. Rhoncus aenean vel elit scelerisque mauris. Pretium viverra suspendisse potenti nullam ac tortor. Nam at lectus urna duis convallis convallis tellus id interdum. Amet tellus cras adipiscing enim eu. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Volutpat ac tincidunt vitae semper quis lectus. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Quis lectus nulla at volutpat diam ut. At imperdiet dui accumsan sit amet. Arcu felis bibendum ut tristique.
-Rutrum quisque non tellus orci. Cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla. Egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices. Quam id leo in vitae. Aliquam sem fringilla ut morbi. Interdum varius sit amet mattis vulputate. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Molestie nunc non blandit massa. Euismod quis viverra nibh cras pulvinar. Rhoncus aenean vel elit scelerisque mauris. Pretium viverra suspendisse potenti nullam ac tortor. Nam at lectus urna duis convallis convallis tellus id interdum. Amet tellus cras adipiscing enim eu. A condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Volutpat ac tincidunt vitae semper quis lectus. Non diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Quis lectus nulla at volutpat diam ut. At imperdiet dui accumsan sit amet. Arcu felis bibendum ut tristique.
-        </Text>
+      <View {...styles.container} {...this.props}>
+        <View {...styles.scrollView}>
+          <View {...styles.scrollViewContent}>
+            <SectionHeader>GLOBALS</SectionHeader>
+            <SectionHeader>DELAY</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Sets the delay time from input to output. The larger the delay time, the larger individual grains can be,
+              and the more opportunity for randomness within each grain engine.
+            </Text>
+            <SectionHeader>CUTOFF</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Both the direct input and the feedback signal run through a filter before entering the grain stage. This parameter
+              sets the cutoff frequency of that filter. The FILTER TYPE button beneath it toggles between Lowpass and Highpass mode.
+            </Text>
+            <SectionHeader>WARP</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Controls a classic pitch warp applied to the grain engine output. Can be used to dial in subtle detune effects or
+              the nostalgic pitch wobble of a classic tape machine.
+            </Text>
+            <SectionHeader>THRESHOLD</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Only takes effect when FREEZE is enabled. While the delay buffer is frozen, the value of the input signal can
+              be used to temporarily disable the freeze, allowing you to capture a new frozen state. This temporary unlock
+              happens as long as the input signal is above the THRESHOLD value set here.
+            </Text>
+            <SectionHeader>MIX</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Your standard wet/dry control. A value of 0% is just the dry input, a value of 100% is just the wet output.
+            </Text>
+            <SectionHeader>GRAIN PARAMETERS</SectionHeader>
+            <SectionHeader>FREQUENCY</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Sets how frequently grains are emitted, and proportionally sets the length of an individual grain. Lower frequency
+              values mean longer grains, but the grain length will automatically be limited to the size of the DELAY parameter.
+            </Text>
+            <SectionHeader>SPRAY</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Introduces randomness when deciding where from within the delay buffer to pick the next grain.
+            </Text>
+            <SectionHeader>SPREAD</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Randomly pans each grain to the left or the right. The SPREAD value sets the maximum pan amount to either side.
+            </Text>
+            <SectionHeader>PITCH</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Sets the playback pitch for each grain.
+            </Text>
+            <SectionHeader>REVERSE</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Sets the probability of playing a grain in reverse. At 100%, all grains will be played in reverse.
+            </Text>
+            <SectionHeader>FEEDBACK</SectionHeader>
+            <Text {...styles.sectionBodyText}>
+              Sets the feedback amount from one grain engine back into itself.
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  container: {
+    'padding-left': 16.0,
+    'padding-right': 16.0,
+  },
+  scrollView: {
+    'flex-direction': 'column',
+    'flex': 1.0,
+    'height': '100%',
+  },
+  scrollViewContent: {
+    'flex-direction': 'column',
+    // This is important. We want the scroll view content to stretch vertically
+    // to contain its children but we need to enforce that it does not get shrunk,
+    // otherwise Yoga will attempt to squeeze it into the size of its container.
+    'flex': 1.0,
+    'flex-shrink': 0.0,
+  },
+  sectionHeader: {
+    'margin-top': 4.0,
+    'margin-bottom': 4.0,
+    'flex-shrink': 0.0,
+  },
+  sectionHeaderText: {
+    'color': Colors.STROKE,
+    'font-size': 18.0,
+  },
+  sectionBodyText: {
+    'color': Colors.STROKE,
+    'font-size': 14.0,
+  },
+};
 
 export default Help;
