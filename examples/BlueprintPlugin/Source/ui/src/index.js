af0e1390c6cb0c0d05ba96abc8ba4a2031b60f95
Nicholas Thompson <ncthom91@gmail.com>
2020-06-25 21:28:56 -0400

    Remove BlueprintPlugin: becoming outdated

--- examples/BlueprintPlugin/Source/ui/src/index.js
+++
@@ -1,8 +0,0 @@
-import "juce-blueprint/cjs/lib/polyfill";
-
-import React from 'react';
-import Blueprint from 'juce-blueprint';
-import App from './App';
-
-// Blueprint.enableMethodTrace();
-Blueprint.render(<App />, Blueprint.getRootContainer());
