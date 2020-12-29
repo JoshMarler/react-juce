0219ff2b5aebe1f4d9c97ad3f99a19def824aec4
Stéphane <albanese.stephane@gmail.com>
2020-12-30 00:36:02 +0100

    Remove APVTS dependency from GenericEditor

--- examples/GainPlugin/PluginProcessor.cpp
+++ examples/GainPlugin/PluginProcessor.cpp
@@ -193,7 +193,7 @@ AudioProcessorEditor* GainPluginAudioProcessor::createEditor()
     File sourceDir = File(__FILE__).getParentDirectory();
     File bundle = sourceDir.getChildFile("jsui/build/js/main.js");
 
-    auto* editor = new blueprint::BlueprintGenericEditor(*this, bundle, &params);
+    auto* editor = new blueprint::BlueprintGenericEditor(*this, bundle);
 
     editor->setResizable(true, true);
     editor->setResizeLimits(400, 240, 400 * 2, 240 * 2);
