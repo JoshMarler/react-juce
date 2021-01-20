589493ca675962bf47f759acc0c1a5744f40b35f
Stéphane Albanese <albanese.stephane@gmail.com>
2021-01-20 23:42:12 +0100

    Implement image cache and onload/onerror callbacks

---
+++ react_juce/core/Utils.h
@@ -0,0 +1,26 @@
+/*
+  ==============================================================================
+
+    Utils.h
+    Created: 20 Jan 2021 10:44:27pm
+
+  ==============================================================================
+*/
+
+#pragma once
+
+namespace blueprint
+{
+    namespace detail
+    {
+        static juce::var makeErrorObject(const juce::String& errorName, const juce::String& errorMessage)
+        {
+            auto* o = new juce::DynamicObject();
+
+            o->setProperty("name", errorName);
+            o->setProperty("message", errorMessage);
+
+            return juce::var(o);
+        }
+    }
+}
