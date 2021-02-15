c783f7a70c1361b8fb05977eebf01bf4fda645ee
Nicholas Thompson <ncthom91@gmail.com>
2021-02-15 16:28:24 -0500

    Remove existing test suite in anticipation of #136

--- react_juce/react_juce.cpp
+++ react_juce/react_juce.cpp
@@ -7,13 +7,13 @@
   ==============================================================================
 */
 
-#ifdef BLUEPRINT_H_INCLUDED
+#ifdef REACTJUCE_H_INCLUDED
  /* When you add this cpp file to your project, you mustn't include it in a file where you've
     already included any other headers - just put it inside a file on its own, possibly with your config
     flags preceding it, but don't include anything else. That also includes avoiding any automatic prefix
     header files that the compiler may be using.
  */
- #error "Incorrect use of the Blueprint cpp file"
+ #error "Incorrect use of the React-JUCE cpp file"
 #endif
 
 // Disable compiler warnings for external source files (yoga)
