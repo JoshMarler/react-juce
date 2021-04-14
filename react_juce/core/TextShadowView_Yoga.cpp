40166265b9dd215bca028e8eec892b240abb1106
Josh Marler <jmarler@inmusicbrands.com>
2021-04-14 11:29:12 +0100

    Fix mac build with strict warnings

--- react_juce/core/TextShadowView_Yoga.cpp
+++ react_juce/core/TextShadowView_Yoga.cpp
@@ -7,27 +7,30 @@ namespace reactjuce
     /** We use this method to measure the size of a given string so that the
      *  text container knows what size to take.
      */
-    YGSize measureTextNode(YGNodeRef node, float width, YGMeasureMode widthMode, float height, YGMeasureMode heightMode)
+    namespace
     {
-        juce::ignoreUnused(widthMode);
-        juce::ignoreUnused(height);
-        juce::ignoreUnused(heightMode);
+        YGSize measureTextNode(YGNodeRef node, float width, YGMeasureMode widthMode, float height, YGMeasureMode heightMode)
+        {
+            juce::ignoreUnused(widthMode);
+            juce::ignoreUnused(height);
+            juce::ignoreUnused(heightMode);
 
-        auto context = reinterpret_cast<TextShadowView*>(YGNodeGetContext(node));
-        auto view = dynamic_cast<TextView*>(context->getAssociatedView());
+            auto context = reinterpret_cast<TextShadowView*>(YGNodeGetContext(node));
+            auto view = dynamic_cast<TextView*>(context->getAssociatedView());
 
-        jassert (view != nullptr);
+            jassert (view != nullptr);
 
-        // TODO: This is a bit of an oversimplification. We have a YGMeasureMode which
-        // is one of three things, "undefined", "exact", or "at-most." Here we're kind of
-        // just ignoring that, and in cases like `white-space: nowrap;` we want to ignore it,
-        // but it would probably be good to get specific for each case.
-        // See https://github.com/facebook/yoga/pull/576/files
-        auto tl = view->getTextLayout(width);
+            // TODO: This is a bit of an oversimplification. We have a YGMeasureMode which
+            // is one of three things, "undefined", "exact", or "at-most." Here we're kind of
+            // just ignoring that, and in cases like `white-space: nowrap;` we want to ignore it,
+            // but it would probably be good to get specific for each case.
+            // See https://github.com/facebook/yoga/pull/576/files
+            auto tl = view->getTextLayout(width);
 
-        return { tl.getWidth(), tl.getHeight() };
+            return { tl.getWidth(), tl.getHeight() };
+        }
     }
-
+    
     //==============================================================================
     class TextShadowView::TextShadowViewPimpl
     {
