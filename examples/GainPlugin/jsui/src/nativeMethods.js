cb58aa24648dfdb675eb2777751ac551caa08c60
jmarler <jmarler.dev@gmail.com>
2024-12-14 12:20:10 +0000

    Remove examples - Move to react-juce-examples repo

--- examples/GainPlugin/jsui/src/nativeMethods.js
+++
@@ -1,8 +0,0 @@
-export const beginParameterChangeGesture = (paramId) =>
-  global.beginParameterChangeGesture(paramId);
-
-export const endParameterChangeGesture = (paramId) =>
-  global.endParameterChangeGesture(paramId);
-
-export const setParameterValueNotifyingHost = (paramId, value) =>
-  global.setParameterValueNotifyingHost(paramId, value);
